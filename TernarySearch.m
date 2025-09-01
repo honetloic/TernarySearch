ace7f73421d8f803c311ac6da802e1d86c0086ea
Loic Honet <loic.honet@ulb.be>
2025-09-01 18:50:47 +0200

    Adding TernarySearchTab for minimization procedure on a data table

--- TernarySearch.m
+++ TernarySearch.m
@@ -6,6 +6,9 @@ BeginPackage["TernarySearch`"];
 TernarySearch::usage="TernarySearch[F,tmin,tmax,\[Epsilon]] finds a minimum of F between tmin and tmax, at precision \[Epsilon]"
 
 
+TernarySearchTab::usage="TernarySearchTab[FTab,\[Delta]tTab] finds a minimum of FTab in \[Delta]tTab range"
+
+
 Begin["Private`"];
 
 
@@ -54,6 +57,63 @@ right=left;
 
 AppendTo[leftSearchValues,{left,F[left]}];
 AppendTo[rightSearchValues,{right,F[right]}];
+<|"leftSearch"->leftSearchValues,"rightSearch"->rightSearchValues|>
+]
+
+
+Options[TernarySearchTab] = {"Monitor" -> False};
+
+
+TernarySearchTab[Ftmp_,\[Delta]tTabtmp_,opts:OptionsPattern[]]:=Module[{monitorQ,F=Ftmp,\[Delta]tTab=\[Delta]tTabtmp,leftPos=1,rightPos=Length[\[Delta]tTabtmp],minVal,leftSearchValues,rightSearchValues,searchValtmp,rightThird,leftThird},
+
+monitorQ=OptionValue["Monitor"];
+
+leftSearchValues={{\[Delta]tTab[[leftPos]],F[\[Delta]tTab[[leftPos]]]}};
+rightSearchValues={{\[Delta]tTab[[rightPos]],F[\[Delta]tTab[[rightPos]]]}};
+
+If[monitorQ,
+Monitor[
+While[leftPos!=rightPos,
+	rightThird=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[rightPos]]-(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]];
+	leftThird=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[leftPos]]+(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]];
+	searchValtmp={{\[Delta]tTab[[leftThird]],F[\[Delta]tTab[[leftThird]]]},{\[Delta]tTab[[rightThird]],F[\[Delta]tTab[[rightThird]]]}};
+	If[leftPos==rightPos-1,
+		If[First@PositionSmallest@searchValtmp[[;;,2]]==1,
+			leftPos=leftPos;
+			rightPos=leftPos];
+		If[First@PositionSmallest@searchValtmp[[;;,2]]==2,
+			leftPos=rightPos;
+			rightPos=rightPos],
+		If[searchValtmp[[1,2]]<searchValtmp[[2,2]],
+			rightPos=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[rightPos]]-(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]],
+			leftPos=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[leftPos]]+(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]]
+		]
+];
+AppendTo[leftSearchValues,{\[Delta]tTab[[leftPos]],F[\[Delta]tTab[[leftPos]]]}];
+AppendTo[rightSearchValues,{\[Delta]tTab[[rightPos]],F[\[Delta]tTab[[rightPos]]]}];
+];
+,leftPos-rightPos],
+While[leftPos!=rightPos,
+	rightThird=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[rightPos]]-(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]];
+	leftThird=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[leftPos]]+(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]];
+	searchValtmp={{\[Delta]tTab[[leftThird]],F[\[Delta]tTab[[leftThird]]]},{\[Delta]tTab[[rightThird]],F[\[Delta]tTab[[rightThird]]]}};
+	If[leftPos==rightPos-1,
+		If[First@PositionSmallest@searchValtmp[[;;,2]]==1,
+			leftPos=leftPos;
+			rightPos=leftPos];
+		If[First@PositionSmallest@searchValtmp[[;;,2]]==2,
+			leftPos=rightPos;
+			rightPos=rightPos],
+		If[searchValtmp[[1,2]]<searchValtmp[[2,2]],
+			rightPos=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[rightPos]]-(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]],
+			leftPos=Position[\[Delta]tTab,First@Nearest[\[Delta]tTab,\[Delta]tTab[[leftPos]]+(\[Delta]tTab[[rightPos]]-\[Delta]tTab[[leftPos]])/3]][[1,1]]
+		]
+];
+AppendTo[leftSearchValues,{\[Delta]tTab[[leftPos]],F[\[Delta]tTab[[leftPos]]]}];
+AppendTo[rightSearchValues,{\[Delta]tTab[[rightPos]],F[\[Delta]tTab[[rightPos]]]}];
+]
+];
+
 <|"leftSearch"->leftSearchValues,"rightSearch"->rightSearchValues|>
 ]
 
